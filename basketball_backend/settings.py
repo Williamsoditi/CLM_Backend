0bf641669fa612d2d1a9350b1cfb10ba66952390
Williamsoditi <williamsoditi99@gmail.com>
2026-01-23 04:26:45 +0300

    Feat : Tweaks settings

--- basketball_backend/settings.py
+++ basketball_backend/settings.py
@@ -92,7 +92,10 @@ WSGI_APPLICATION = 'basketball_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600, 
+        ssl_require={'sslmode': 'require'})
 }
 
 
